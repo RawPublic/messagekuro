27cf2274e1b16c48bf547a7d8c6d7ac65cdd796c
RawPublic <95525465+RawPublic@users.noreply.github.com>
2022-04-16 05:07:25 +0700

    Update versionhub.lua

--- versionhub.lua
+++ versionhub.lua
@@ -1,4 +1,4 @@
-getgenv().Version = "<font color='rgb(255,0,0)'><b>NugrahaFX</b></font><font size ='11' color='rgb(255,250,250)'> HUB</font>"
+getgenv().Version = "<font color='rgb(255,0,0)'><b>NugrahaFX</b></font><font size ='11' color='rgb(255,250,250)'> HUB</font><font size ='15' color='rgb(255,250,250)'> |</font><font size ='15' color='rgb(255,250,250)'> AUTO HOP ALL BOSS</font>"
 local Ui = game:GetService("CoreGui"):FindFirstChild(getgenv().Version)
 if  Ui then
    Ui:Destroy()
