af3d99aeeb8645a8d0e41692c18092c667c70e4d
RawPublic <95525465+RawPublic@users.noreply.github.com>
2022-04-16 05:03:40 +0700

    Create versionhub.lua

---
+++ versionhub.lua
@@ -0,0 +1,5 @@
+getgenv().Version = "<font color='rgb(255,0,0)'><b>NugrahaFX</b></font><font size ='11' color='rgb(255,250,250)'> HUB</font>"
+local Ui = game:GetService("CoreGui"):FindFirstChild(getgenv().Version)
+if  Ui then
+   Ui:Destroy()
+end
